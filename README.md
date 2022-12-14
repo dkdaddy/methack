35fbb9dfe505849ebea775cc9ada6bb5cc03b84f
TS Bob <bob.cook.1966@gmail.com>
2022-12-14 08:43:34 +0000

    random

--- README.md
+++ README.md
@@ -1,7 +1,74 @@
-drawing logic
+# Random stuff
+
+## High Level Activities
+```
+Home  
+  Today (BTDY)
+  Top Stories (TOP)
+  News Read Story  (NSN) 
+  My News (MYN)  Equities (WEI)
+  Economic Events (ECO)  Worksheets   
+  [+ any other widgets on home which has a "More >" action]
+Search
+IB
+  Start/Find Chat
+  Read Chat  Start/Find/Read (iPad mode uses split view so user sees both things together)
+  New Chat
+  New Blast
+  Settings
+  What's new tour
+  View Chat Details
+  Participants
+  Forward Post
+  Reply to Post
+Alerts
+People
+Front Page
+Securities
+Research
+TV
+Radio
+TW
+FILE
+NOTE
+```
+
+## Visualisations
+
+For basic graphs we could use https://developers.google.com/chart/interactive/docs/gallery but it can be fiddly to get exactly what you want. if the examples are good enough and you just want a different dataset it is easy though.
+
+Google Analytics
+https://www.orbitmedia.com/blog/user-flow-google-analytics/
+https://digitalcommunications.wp.st-andrews.ac.uk/2016/11/15/using-the-users-flow-report-in-google-analytics/
+https://yoast.com/users-flow-in-google-analytics/
+
+This seems a clone of GA
+https://mixpanel.com/blog/user-flow-analysis/
+
+Good example showing how loops are represented in GA User Flow analytics
+https://digitalcommunications.wp.st-andrews.ac.uk/files/2016/10/users-flow-step1.png
+
+Note how "/" and "/index.html" appears in multiple columns
+
+Note also the (>100 more pages) bucketsI think the way this works is user picks a start page (in our case Activity), or a session attribute (e.g. country), and then we group every session based on first activity, second, third, etc. such that we have a tree (non a graph - there is no recombining paths) where activities are represented as nodes (and can appear repeated at multiple levels) and the tree depth corresponds to first activity, second, third, etc.
+
+Chart style is called Sankey
+https://d3-graph-gallery.com/sankey.htm
+https://developers.google.com/chart/interactive/docs/gallery/sankeyl
+
+
+
+
+## Notes for drawing Sankey diagram
+
 based on https://digitalcommunications.wp.st-andrews.ac.uk/files/2016/10/users-flow-step1.png
+
 See also https://mixpanel.com/blog/user-flow-analysis/ (Starting=Open Message, Ending=Any Event, Users In 2 Cohorts, Time Last 30 days)
 
+https://www.w3schools.com/graphics/svg_grad_linear.asp
+https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Gradients
+https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
+```
 for each stage "1st interaction,..."
  X sessions, y drop-offs
  for each activity, ranked by count
@@ -13,19 +80,6 @@ height of box proportional to count (compute scaling factor based on total sessi
 out flows, multiple, one per connection, with proportional 
 red gradient drop off, width proportional to drop off rate
 
-
-
  Interactivity
  Filters on sessions - time, device type, device, UUID, cohort, 
-
- Aggregate data format 
-   array of activity, count, child activities
-
-uuid, start time, end time, device, other stuff
-fake data generator 
-    per session
-    pick random activity, pick second random, third, 
-    skew with activity popularity and transition popularities 
-    IB 50% of time, MSG 20%, => foreach activity, Math.random()>activity.x => pick it
-    IB->MSG 10% 
-    nothing picked, drop-out
+```
--- analytics.ts
+++ analytics.ts
@@ -1,8 +1,10 @@
 import http from "node:http"
 import fs from "fs"
+import { getFips } from "node:crypto"
+import { nodeModuleNameResolver } from "typescript"
 
-type ActivityName = 'IB'|'MSG'|'BTDY'
-type ActionName = 'MSG.send'|'IB.reply'|'IB.fwd'
+type ActivityName = 'IB'|'MSG'|'BTDY'|'NSN'
+type ActionName = 'MSG.compose'|'IB.reply'|'IB.fwd'
 type InteractionName = ActivityName|ActionName
 
 class Activity {
@@ -40,12 +42,60 @@ class Session {
     public user:string
     public eventTimeline: ActivityOrAction[] // time ordered
 }
+const pickRandomActivity = ():Activity|undefined => {
+    const now = new Date()
+    if (Math.random()>0.8) return new Activity("BTDY", now, now)
+    if (Math.random()>0.8) return new Activity("IB", now, now)
+    if (Math.random()>0.8) return new Activity("MSG", now, now)
+    return undefined
+}
+const randomData = (quantity:number):Session[] => {
+    let result:Session[] = []
 
+    for (let n=0; n<quantity; n++) {
+        const user = "bob"
+        const device = "X-1234"
+        const start = new Date()
+        const end = new Date()
+        const session = new Session(user, device,start, end)
+        let lastEvent:ActivityOrAction|undefined=undefined
+        for (let t=0;t<10; t++) {
+            const event:ActivityOrAction|undefined = pickRandomActivity()
+            if (event && (!lastEvent || event.name != lastEvent.name)) {
+                session.eventTimeline.push(event)
+                lastEvent = event
+            }
+        }
+        result.push(session)
+    }
+    return result
+
+}
+const reduceSessions = (sessions:Session[]):AggregateActiviyOrAction[] => {
+    let root:AggregateActiviyOrAction = new AggregateActiviyOrAction('NSN',0,[]) // not real, name does not matter
+
+    sessions.forEach(session => {
+        let currentNode:AggregateActiviyOrAction = root
+        session.eventTimeline.forEach(event => {
+            // place this event in this level and move to next level
+            let item = currentNode.next.find((node) => node.interaction==event.name)
+            if (!item) {
+                item = new AggregateActiviyOrAction(event.name,0,[])
+                currentNode.next.push(item)
+            }
+            item.count++
+            currentNode = item
+        })
+    })
+    let result = root.next
+    result.sort((a,b) => a.count-b.count) // descending size 
+    return result
+}
 class AggregateActiviyOrAction {
-    constructor(interation:InteractionName, count:number) {
+    constructor(interation:InteractionName, count:number, next:AggregateActiviyOrAction[]=[]) {
         this.interaction = interation
         this.count = count
-        this.next = []
+        this.next = next
     }
     public count:number
     public interaction:InteractionName
@@ -57,7 +107,7 @@ const renderHTML = (roots:AggregateActiviyOrAction[]):string => {
     let html = ""
     const sessions = roots.reduce((prev, curr) => {return prev+curr.count}, 0)
     const dropOuts = sessions - roots.reduce((prev, curr) => {return prev + curr.next.reduce((p,c)=>p+c.count, 0)},0)
-    html += `Sessions ${sessions} Drop-outs ${dropOuts}`
+    html += `Sessions ${sessions} Drop-outs ${dropOuts}<hr><br>`
     roots.forEach(child => {
         html += renderActivity(child,1)
     })
@@ -65,7 +115,7 @@ const renderHTML = (roots:AggregateActiviyOrAction[]):string => {
 }
 
 const renderActivity = (activity:AggregateActiviyOrAction, level:number):string => {
-    let html = ""
+    let html = "<div>"
     for (let n=0; n<level; n++) {
         html += "&nbsp;&nbsp;&nbsp;&nbsp;" // indent
     }
@@ -73,9 +123,22 @@ const renderActivity = (activity:AggregateActiviyOrAction, level:number):string 
     activity.next.forEach(child => {
         html += renderActivity(child,level+1)
     })
+    html += "</div>"
     return html
 }
 
+const getFakeData = () => {
+    const fwd = new AggregateActiviyOrAction("IB.fwd", 23211)
+    const reply = new AggregateActiviyOrAction("IB.reply", 2011)
+    const nsn = new AggregateActiviyOrAction("NSN", 18277)
+    const msgCompose = new AggregateActiviyOrAction("MSG.compose", 8772)
+    return [
+        new AggregateActiviyOrAction("IB", 27232,[fwd, reply]),
+        new AggregateActiviyOrAction("MSG", 42227,[msgCompose]),
+        new AggregateActiviyOrAction("BTDY", 19327, [nsn]),
+    ]
+    
+}
 const hostname = "" //os.hostname();
 const port = 8000
 
@@ -89,7 +152,11 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200
     console.log(req.method, req.url)
     if (req.url?.startsWith("/")) {
-        const html = "hello"
+        // const data = getFakeData()
+        const sessions = randomData(100)
+        const data = reduceSessions(sessions)
+        const html = renderHTML(data)
+        res.setHeader("Content-Type", "text/html")
         res.end(html)
     
     } else {
